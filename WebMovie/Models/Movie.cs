36083a728d4d3f3aeeac88390260c4ed5f56a5e8
Jodi Noordiansyah <jodinoordiansyah@gmail.com>
2024-03-04 09:23:49 +0700

    Init

---
+++ WebMovie/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+        [Required]
+        public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString ="{0:N0}")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ WebMovie/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+using WebMovie.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebMovieContext") ?? throw new InvalidOperationException("Connection string 'WebMovieContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+var supportedCultures = new[]{
+    new CultureInfo("en-US")
+};
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en-US"),
+    SupportedCultures = supportedCultures,
+    FallBackToParentCultures = false
+});
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
+
+app.Run();
